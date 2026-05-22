72dd4302283b05bd9c0c455a76f870cede7e2290
alemusewnet210-netizen <alemusewnet210-netizen@gmail.com>
2026-05-22 09:45:46 -0700

    alex edit the contact.php

--- contact.php
+++ contact.php
@@ -122,3 +122,4 @@ try {
     http_response_code(500);
     echo json_encode(['status' => 'error', 'message' => 'Message saved, but email could not be sent. Mailer Error: ' . $mail->ErrorInfo]);
 }
+//hi alemu
